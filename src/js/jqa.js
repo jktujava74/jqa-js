e73e45dbf10ce60d864a6dd4ded46129fcbf08a4
jktu <myway0@mail.ru>
2021-07-28 11:24:40 +0500

    use axios as dependency, not plain

--- src/js/jqa.js
+++ src/js/jqa.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import routers from './rout'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import Category from "./components/Category";
+
+Vue.use(VueAxios, axios)
 
 const v = new Vue({
     el: '#container',
